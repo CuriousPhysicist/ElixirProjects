871dedf73366fd1715d4c9ee2d11bca4c3b11fbf
Andrew Hampson <andrew.hampson@itison.com>
2019-12-11 09:30:47 +0000

    Initial push, code for first demo in pseudo-code

---
+++ AlchemistCamp/guessing_game/guessing_game.ex
@@ -0,0 +1,8 @@
+defmodule GuessingGame do
+  # guess between a low number and a high number -> guess middle number
+  # tell the user our guess
+  # "yes" -> game over
+  # "bigger" -> bigger(low, high)
+  # "smaller" -> smaller(low, high)
+  # anything else -> tell user to enter a valid response
+end
